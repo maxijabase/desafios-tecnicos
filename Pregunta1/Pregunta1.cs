219fcc97784dbdd4fcfb46e8970e104824805137
Maxi Jabase <maxijabase@gmail.com>
2021-08-30 10:56:43 -0300

    initial commit

---
+++ Pregunta1/Pregunta1.cs
@@ -0,0 +1,60 @@
+﻿/*
+1) Crear una función que devuelva una versión simplificada de una fracción
+   Ejemplos
+   Simplificar("4/6") = "2/3"
+   Simplificar("10/11) = "10/11"
+   Simplificar("100/400") = "10/4"; (<-- "1/4")
+   Notas: una fracción simplificada no tiene divisores comunes mínimos (excepto 1) entre el
+   numerador y el denominador. Por ejemplo, 4/6 no esta simplificada, ya que tanto 4 y 6
+   comparten 2 como factor
+   Si una fracción puede ser transformada en entero, también debe tenerse en cuenta
+ */
+
+using System;
+
+namespace DesafioTecnico
+{
+    public class Pregunta1
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                // Capturo el input para procesarlo en Simplificar()
+                Console.Write("Inserte una fracción con el formato numero/numero. Si es posible, será devuelta simplificada: ");
+                try
+                {
+                    Console.WriteLine($"Resultado: {Simplificar(Console.ReadLine())}");
+                }
+                // Capturo cualquier error genéricamente para reducir la complejidad de la validación del input
+                catch (Exception)
+                {
+                    Console.WriteLine($"Entrada incorrecta.");
+                    continue;
+                }
+            }
+        }
+
+        private static string Simplificar(string fraccion)
+        {
+            // Extraigo y parseo numerador y denominador del input
+            var numer = int.Parse(fraccion.Split('/')[0]);
+            var denom = int.Parse(fraccion.Split('/')[1]);
+            // Obtengo MCD mediante algoritmo de Euclides
+            var mcd = ObtenerMCD(numer, denom);
+
+            // Si el MCD no fue 0, no es una fracción entera, reducimos
+            // Sino, MCD 0 indica que es una fracción entera, dividimos normalmente
+            return mcd == 0 ? $"{numer / denom}" : $"{numer / mcd}/{denom / mcd}";
+        }
+
+        private static int ObtenerMCD(int numer, int denom)
+        {
+            // Aplicando mcd(a, b) = mcd(b, a mod b) recursivamente,
+            // llegamos al resto que resulta ser el MCD, o continuamos 
+            // pidiéndolo hasta que lo obtenemos
+            var remainder = numer % denom;
+            return remainder == 0 || denom % remainder == 0 ? remainder : ObtenerMCD(denom, numer % denom);
+        }
+    }
+}
---
+++ Pregunta2/Pregunta2.cs
@@ -0,0 +1,124 @@
+﻿/*
+2) Validar Nombres
+   Tener en cuenta las siguientes definiciones
+    a) El termino ingresado pueden ser iniciales o palabras completas
+    b) Una inicial es solo un caracter mas un punto
+    c) Una palabra se comprende de 2 o mas caracteres, sin punto
+
+   Reglas
+    a) Tanto las iniciales como las palabras completas deben estar capitalizadas (la primera letra
+     en mayúsculas)
+    b) Las iniciales deben terminar en punto (.)
+    c) Solo habrán 2 o 3 términos en el ingreso. Es decir, o dos nombres y un apellido o solo un
+     nombre y un apellido
+    d)Si se ingresan dos nombres y un apellido, los dos primeros pueden ser iniciales, o solo el
+     segundo. Nunca puede ser una inicial el primer nombre y no el segundo
+    e) El apellido siempre debe ser una palabra completa
+
+ */
+
+using System;
+using System.Linq;
+
+namespace DesafioTecnico
+{
+    public class Pregunta2
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Inserte el nombre: ");
+                    Console.WriteLine($"El formato del nombre es {(ValidarNombre(Console.ReadLine()) ? "VÁLIDO" : "INVÁLIDO")}.\n");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Entrada incorrecta.\n");
+                    continue;
+                }
+            }
+        }
+
+        private static bool ValidarNombre(string input)
+        {
+            var nombres = input.Split(' ');
+            return ComienzanConMayuscula(nombres) &&
+                   CantidadCorrectaDePalabras(nombres) &&
+                   TienePalabrasValidas(nombres) &&
+                   TieneInicialesValidas(nombres) &&
+                   TieneApellidoValido(nombres) &&
+                   OrdenCorrectoDeIniciales(nombres);
+                   
+        }
+
+        #region Reglas Generales
+
+        private static bool CantidadCorrectaDePalabras(string[] nombres)
+        {
+            // No deben ser menos de 1 y más de 3 palabras
+            return nombres.Length > 1 && nombres.Length <= 3;
+        }
+
+        private static bool ComienzanConMayuscula(string[] nombres)
+        {
+            foreach (var parte in nombres)
+            {
+                // Todas las partes del input deben comenzar con mayúscula
+                if (parte[0].ToString() != parte[0].ToString().ToUpper())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool TienePalabrasValidas(string[] nombres)
+        {
+            foreach (var parte in nombres)
+            {
+                // Todas las palabras válidas deben tener igual o más de 2 caracteres alfabéticos
+                if (parte.Length == 1 || (!parte.Any(x => char.IsLetter(x)) && parte.Length > 1))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool TieneInicialesValidas(string[] nombres)
+        {
+            foreach (var parte in nombres)
+            {
+                // Ninguna inicial válida contiene un punto sin contener 2 caracteres en total
+                if (parte.Length != 2 && parte.Contains("."))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region Reglas Particulares
+
+        private static bool OrdenCorrectoDeIniciales(string[] nombres)
+        {
+            // El input ingresado es inválido si consta de 3 partes,
+            // pero el nombre intenta ser una inicial mientras que el segundo nombre no
+            return !(nombres.Length == 3 && nombres[0].Contains(".") && !nombres[1].Contains("."));
+        }
+
+
+        private static bool TieneApellidoValido(string[] nombres)
+        {
+            // El apellido será válido si no intenta ser una inicial
+            var apellido = nombres[^1];
+            return !(apellido.Contains("."));
+        }
+
+        #endregion
+    }
+}
