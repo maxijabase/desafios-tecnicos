540fce212b376f3adc66675d2cc9b40a79814386
Maxi Jabase <maxijabase@gmail.com>
2021-10-21 16:26:07 -0300

    actualizo a .NET 6 y modernizo sintaxis de C#10

--- Pregunta1/Pregunta1.cs
+++ Pregunta1/Pregunta1.cs
@@ -12,49 +12,48 @@
 
 using System;
 
-namespace DesafioTecnico
+namespace DesafioTecnico;
+
+public class Pregunta1
 {
-    public class Pregunta1
+    public static void Main()
     {
-        public static void Main()
+        while (true)
         {
-            while (true)
+            // Capturo el input para procesarlo en Simplificar()
+            Console.Write("Inserte una fracción con el formato numero/numero. Si es posible, será devuelta simplificada: ");
+            try
             {
-                // Capturo el input para procesarlo en Simplificar()
-                Console.Write("Inserte una fracción con el formato numero/numero. Si es posible, será devuelta simplificada: ");
-                try
-                {
-                    Console.WriteLine($"Resultado: {Simplificar(Console.ReadLine())}");
-                }
-                // Capturo cualquier error genéricamente para reducir la complejidad de la validación del input
-                catch (Exception)
-                {
-                    Console.WriteLine($"Entrada incorrecta.");
-                    continue;
-                }
+                Console.WriteLine($"Resultado: {Simplificar(Console.ReadLine())}");
+            }
+            // Capturo cualquier error genéricamente para reducir la complejidad de la validación del input
+            catch (Exception)
+            {
+                Console.WriteLine($"Entrada incorrecta.");
+                continue;
             }
         }
+    }
 
-        private static string Simplificar(string fraccion)
-        {
-            // Extraigo y parseo numerador y denominador del input
-            var numer = int.Parse(fraccion.Split('/')[0]);
-            var denom = int.Parse(fraccion.Split('/')[1]);
-            // Obtengo MCD mediante algoritmo de Euclides
-            var mcd = ObtenerMCD(numer, denom);
+    private static string Simplificar(string fraccion)
+    {
+        // Extraigo y parseo numerador y denominador del input
+        var numer = int.Parse(fraccion.Split('/')[0]);
+        var denom = int.Parse(fraccion.Split('/')[1]);
+        // Obtengo MCD mediante algoritmo de Euclides
+        var mcd = ObtenerMCD(numer, denom);
 
-            // Si el MCD no fue 0, no es una fracción entera, reducimos
-            // Sino, MCD 0 indica que es una fracción entera, dividimos normalmente
-            return mcd == 0 ? $"{numer / denom}" : $"{numer / mcd}/{denom / mcd}";
-        }
+        // Si el MCD no fue 0, no es una fracción entera, reducimos
+        // Sino, MCD 0 indica que es una fracción entera, dividimos normalmente
+        return mcd == 0 ? $"{numer / denom}" : $"{numer / mcd}/{denom / mcd}";
+    }
 
-        private static int ObtenerMCD(int numer, int denom)
-        {
-            // Aplicando mcd(a, b) = mcd(b, a mod b) recursivamente,
-            // llegamos al resto que resulta ser el MCD, o continuamos 
-            // pidiéndolo hasta que lo obtenemos
-            var remainder = numer % denom;
-            return remainder == 0 || denom % remainder == 0 ? remainder : ObtenerMCD(denom, numer % denom);
-        }
+    private static int ObtenerMCD(int numer, int denom)
+    {
+        // Aplicando mcd(a, b) = mcd(b, a mod b) recursivamente,
+        // llegamos al resto que resulta ser el MCD, o continuamos 
+        // pidiéndolo hasta que lo obtenemos
+        var remainder = numer % denom;
+        return remainder == 0 || denom % remainder == 0 ? remainder : ObtenerMCD(denom, numer % denom);
     }
 }
